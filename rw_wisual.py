4215728d755b22e89ac9532c51633a348291851e
dshebeko20 <dshebeko20@gmail.com>
2025-10-30 13:49:40 +0400

    A few wanderings.

--- rw_wisual.py
+++ rw_wisual.py
@@ -2,14 +2,20 @@ import matplotlib.pyplot as plt
 
 from random_walk import Randomwalk
 
-# Создание случайного блуждания.
-rw = Randomwalk()
-rw.fill_walk()
+# Новые блуждания создаются до тех пор, пока программа остаётся активной.
+while True:
+    # Создание случайного блуждания.
+    rw = Randomwalk()
+    rw.fill_walk()
 
-# Нанесение точек на диаграмму.
-plt.style.use('classic')
+    # Нанесение точек на диаграмму.
+    plt.style.use('classic')
 
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-ax.set_aspect('equal')
-plt.show()
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+    ax.set_aspect('equal')
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n):")
+    if keep_running == 'n':
+        break
