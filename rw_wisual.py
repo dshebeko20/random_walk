f38e6b8b81a590becb0942cf329b22dfcf309f38
dshebeko20 <dshebeko20@gmail.com>
2025-10-30 15:49:37 +0400

    Changing the chart size.

--- rw_wisual.py
+++ rw_wisual.py
@@ -5,15 +5,27 @@ from random_walk import Randomwalk
 # Новые блуждания создаются до тех пор, пока программа остаётся активной.
 while True:
     # Создание случайного блуждания.
-    rw = Randomwalk()
+    rw = Randomwalk(50_000)
     rw.fill_walk()
 
     # Нанесение точек на диаграмму.
     plt.style.use('classic')
 
-    fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    fig, ax = plt.subplots(figsize=(15, 9))
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, 
+        edgecolors='none', s=1)
     ax.set_aspect('equal')
+
+    # Выделение первой и последней точек.
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
+        s=100)
+    
+    # Удаление осей.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n):")
